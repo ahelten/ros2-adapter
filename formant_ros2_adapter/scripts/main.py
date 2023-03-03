ed3aff830ffe0a7a1b6005d2b07461db15cc151d
ryancallier22 <97465438+ryancallier22@users.noreply.github.com>
2023-03-03 10:54:16 -0800

    Added Teleop logging

--- formant_ros2_adapter/scripts/main.py
+++ formant_ros2_adapter/scripts/main.py
@@ -543,6 +543,7 @@ class Adapter:
         msg.angular.z = value.angular.z
 
         publisher.publish(msg)
+        print(str(msg))
 
     def publish_bool(self, value, publisher):
         msg = Bool()
